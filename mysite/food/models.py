7836120fa8999e356842abf0ee81ccefefaddaf1
sheshmaninigam <nigama451@gmail.com>
2023-09-05 15:45:29 +0530

    commit:10

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,3 +6,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
