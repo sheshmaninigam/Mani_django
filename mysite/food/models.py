ebed971cd340c06656a660ced9d2b871d248582f
sheshmaninigam <nigama451@gmail.com>
2023-09-12 16:42:57 +0530

    commit:26

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,6 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://img.freepik.com/free-vector/fast-food-black-background-poster_1284-14589.jpg?w=2000"
+    )
 
     def __str__(self):
         return self.item_name
