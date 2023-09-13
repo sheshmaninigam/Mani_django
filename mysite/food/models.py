70bf83c4c88d618cbfa16c91f364579cb31df8a1
sheshmaninigam <nigama451@gmail.com>
2023-09-13 16:39:47 +0530

    commit:30

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
+    prod_code = models.IntegerField(default=100)
+    for_user = models.CharField(
+        max_length=100, 
+        default="xyz"
+        )
     item_name = models.CharField(max_length=50)
-    item_desc = models.CharField(max_length=300)
+    item_desc = models.CharField(
+        max_length=500,
+        default='''Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestias nobis incidunt laudantium molestiae recusandae
+officiis! Praesentium magni dicta ipsa? Quisquam earum explicabo iusto quae est id sapiente ad natus.'''
+        )
     item_price = models.IntegerField()
     item_image = models.CharField(
         max_length=500,
