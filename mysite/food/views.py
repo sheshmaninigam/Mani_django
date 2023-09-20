ea6eabfd164404ef59e64033d8fd2584d266b076
sheshmaninigam <nigama451@gmail.com>
2023-09-20 16:21:22 +0530

    commit:37

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -42,3 +42,16 @@ def update_item(request,id):
         form.save()
         return redirect("food:index")
     return render(request,"food/item-form.html",context)
+
+def delete_item(request, id):
+    item = Item.objects.get(pk=id)
+
+    context = {
+        'item':item
+    }
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("food:index")
+    
+    return render(request, "food/item-delete.html", context)
