810cd2ba51119ca247c776c2b4b8063b2c62434d
sheshmaninigam <nigama451@gmail.com>
2023-10-05 16:22:01 +0530

    commit:58

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile.jpg', upload_to='profile_picture')
+    image = models.ImageField(default='profilepic.jpg', upload_to='profile_picture')
     location  = models.CharField(max_length=100)
 
     def __str__(self):
