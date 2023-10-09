7681fb2997e7c84ad65d61206978b6a7ada69445
sheshmaninigam <nigama451@gmail.com>
2023-10-09 15:39:26 +0530

    commit:60

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from food.models import Item
 from food.forms import ItemForm
+from django.views.generic.list import ListView
 
 # Create your views here.
 
+
+# function based index view
 
 def index(request):
     itemlist = Item.objects.all()
@@ -13,12 +16,29 @@ def index(request):
     }
     return render(request, 'food/index.html', context)
 
+
+# class based index view
+
+class IndexClassView(ListView):
+    model = Item
+    context_object_name = "itemlist"
+    template_name = "food/index.html"
+
+
+# function based detail view
+
+
 def detail(request,item_id):
     item =Item.objects.get(pk=item_id)
     context = {
          "item":item
     }
     return render(request, "food/detail.html", context)
+
+
+
+# function based create_item view
+
 
 def create_item(request):
      form = ItemForm(request.POST or None)
@@ -30,6 +50,11 @@ def create_item(request):
          "form":form
      }
      return render(request,"food/item-form.html",context)
+
+
+
+# function based update_item view
+
 
 def update_item(request,id):
     item = Item.objects.get(pk=id)
@@ -43,6 +68,10 @@ def update_item(request,id):
         form.save()
         return redirect("food:index")
     return render(request,"food/item-form.html",context)
+
+
+# function based delete_item view
+
 
 def delete_item(request, id):
     item = Item.objects.get(pk=id)
