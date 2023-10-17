639664dc628eea6cc01427a4da0de7bc8462ec48
sheshmaninigam <nigama451@gmail.com>
2023-10-17 15:42:58 +0530

    commit:70

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -28,3 +28,21 @@ officiis! Praesentium magni dicta ipsa? Quisquam earum explicabo iusto quae est 
 
     def __str__(self):
         return self.item_name
+    
+
+class History(models.Model):
+    user_name = models.CharField(max_length=100)
+    prod_ref = models.IntegerField(default=100)
+    item_name = models.CharField(max_length=200)
+    op_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(
+            (
+                self.prod_ref,
+                self.user_name,
+                self.item_name,
+                self.op_type
+            )
+        )
+
