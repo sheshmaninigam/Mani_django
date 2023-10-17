0dab4f77a48243f521230dc63ddac02fec806914
sheshmaninigam <nigama451@gmail.com>
2023-10-17 16:41:51 +0530

    commit:72

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -109,7 +109,20 @@ def update_item(request,id):
     
     if form.is_valid():
         form.save()
+        
+    
+
+    
+        Obj_History = History(
+            user_name = request.user.username,
+            prod_ref = form.instance.prod_code,
+            item_name = request.POST.get("item_name"),
+            op_type = "Updated"
+        )
+        Obj_History.save()
+
         return redirect("food:index")
+
     return render(request,"food/item-form.html",context)
 
 
