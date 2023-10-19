f07725212db9e57a4f30e733132841489b4d7dde
sheshmaninigam <nigama451@gmail.com>
2023-10-19 16:36:15 +0530

    commit:78

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -46,3 +46,4 @@ class History(models.Model):
             )
         )
 
+
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -4,6 +4,7 @@ from users.forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from users.models import CusOrders
 
 # Create your views here.
 
@@ -71,5 +72,26 @@ def logout_view(request):
 @login_required
 def profilepage(request):
     return render(request,'users/profile.html')
+
+
+def Orders(request, id, pdcd, user):
+
+
+    context = {
+        "pdcd":pdcd,
+        "user":user
+    }
+
+
+    if request.method == "POST":
+       obj_CusOrders = CusOrders(
+           prod_code = pdcd,
+           user = user,
+           quantity = request.POST.get("qty")
+        )
+       obj_CusOrders.save()
+       return redirect("food:detail", item_id=id)
+        
+    return render(request, "users/orders.html", context)
 
    
